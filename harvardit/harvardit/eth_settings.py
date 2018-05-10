a93ddc7a59c70c4691a5bb2f8057e85cee1d64c6
Alfonso de la Rocha <alfon.rocha@gmail.com>
2018-05-10 08:19:26 +0200

    Added eth_settings

---
+++ harvardit/harvardit/eth_settings.py
@@ -0,0 +1,13 @@
+eth_host = 'http://localhost'
+eth_port = 22000
+
+default_pass = 'Passw0rd_1234'
+default_gas = 4000000
+
+initial_ether_transfer = 0
+
+# Only if initial_ether_transfer > 0
+coinbase_address = "*"
+coinbase_pass = "Passw0rd"
+
+POA = True
